97039b6ba64a205f8f977a4bbfd74ab9f4b460cb
GersonRS <gersonrodriguessantos8@gmail.com>
2026-02-08 01:00:36 -0300

    feat: add custom JupyterHub templates and profile_options
    - Add initContainers to clone custom templates from GitHub
    - Add extraVolumes/extraVolumeMounts for custom templates
    - Update profileList with profile_options for resources and images
    - Update Helm chart to v4.3.2
    - Add cull configuration

--- locals.tf
+++ locals.tf
@@ -10,6 +10,54 @@ locals {
         }
       }
       hub = {
+        initContainers = [
+          {
+            name  = "git-clone-templates"
+            image = "alpine/git"
+            securityContext = {
+              runAsUser    = 1000
+              runAsGroup   = 1000
+              runAsNonRoot = true
+            }
+            command = ["/bin/sh", "-c"]
+            args = [
+              "cd /tmp && git clone --branch=main https://github.com/GersonRS/jupyterhub-templates.git && cp -r jupyterhub-templates/templates/* /templates && cp -r jupyterhub-templates/extra-assets/* /extra-assets"
+            ]
+            volumeMounts = [
+              {
+                name      = "custom-templates"
+                mountPath = "/templates"
+              },
+              {
+                name      = "custom-templates-extra-assets"
+                mountPath = "/extra-assets"
+              }
+            ]
+          }
+        ]
+        extraVolumes = [
+          {
+            name     = "custom-templates"
+            emptyDir = {}
+          },
+          {
+            name     = "custom-templates-extra-assets"
+            emptyDir = {}
+          }
+        ]
+        extraVolumeMounts = [
+          {
+            name      = "custom-templates"
+            mountPath = "/usr/local/share/jupyterhub/custom_templates"
+          },
+          {
+            name      = "custom-templates-extra-assets"
+            mountPath = "/usr/local/share/jupyterhub/static/extra_assets"
+          }
+        ]
+        extraConfig = {
+          templates = "c.JupyterHub.template_paths = ['/usr/local/share/jupyterhub/custom_templates/']"
+        }
         extraEnv = {
           OAUTH_TLS_VERIFY = "0"
         }
@@ -55,104 +103,88 @@ locals {
           AWS_ENDPOINT               = "http://${var.storage.endpoint}"
           AWS_ACCESS_KEY_ID          = "${var.storage.access_key}"
           AWS_SECRET_ACCESS_KEY      = "${var.storage.secret_access_key}"
-          AWS_REGION                 = "eu-west-1",
-          AWS_ALLOW_HTTP             = "true",
-          AWS_S3_ALLOW_UNSAFE_RENAME = "true",
+          AWS_REGION                 = "eu-west-1"
+          AWS_ALLOW_HTTP             = "true"
+          AWS_S3_ALLOW_UNSAFE_RENAME = "true"
           RAY_ADDRESS                = var.ray != null ? "ray://${var.ray.endpoint}:10001" : null
         }
         profileList = [
-          # {
-          #   display_name = "DataScience",
-          #   slug         = "datascience",
-          #   description  = "CPU: 2 cores, RAM: 4Gi"
-          #   default      = true,
-          #   profile_options = {
-          #     image = {
-          #       display_name = "Image",
-          #       choices = {
-          #         pytorch = {
-          #           display_name = "Data Science",
-          #           default      = true,
-          #           kubespawner_override = {
-          #             image = "quay.io/jupyter/datascience-notebook:latest"
-          #           }
-          #         },
-          #         tf = {
-          #           display_name = "Spark",
-          #           kubespawner_override = {
-          #             image = "quay.io/jupyter/all-spark-notebook:latest"
-          #           }
-          #         }
-          #       }
-          #     }
-          #   },
-          #   kubespawner_override = {
-          #     cpu_limit = 2,
-          #     mem_limit = "4G",
-          #   }
-          # },
-          # {
-          #   display_name = "DataEngineer",
-          #   slug         = "dataengineer",
-          #   profile_options = {
-          #     memory = {
-          #       display_name = "Memory",
-          #       choices = {
-          #         "2Gi" = {
-          #           display_name = "2GB",
-          #           kubespawner_override = {
-          #             mem_limit = "2G"
-          #           }
-          #         },
-          #         "4Gi" = {
-          #           display_name = "4G",
-          #           kubespawner_override = {
-          #             mem_limit = "4G"
-          #           }
-          #         }
-          #       }
-          #     },
-          #     cpu = {
-          #       display_name = "CPUs",
-          #       choices = {
-          #         "2" = {
-          #           display_name = "2 CPUs",
-          #           kubespawner_override = {
-          #             cpu_limit     = 2,
-          #             cpu_guarantee = 1.8,
-          #           }
-          #         },
-          #         "4" = {
-          #           display_name = "4 CPUs",
-          #           kubespawner_override = {
-          #             cpu_limit     = 4,
-          #             cpu_guarantee = 3.5,
-          #           }
-          #         }
-          #       }
-          #     },
-          #   },
-          #   kubespawner_override = {
-          #     image = "quay.io/jupyter/all-spark-notebook:latest",
-          #   }
-          # },
           {
-            display_name = "Data Science",
-            slug         = "datascience",
-            description  = "CPU: 1 core, RAM: 2Gi"
-            kubespawner_override = {
-              image     = "quay.io/jupyter/datascience-notebook:latest",
-              cpu_limit = 1,
-              mem_limit = "2G",
-            }
-          },
-          {
-            display_name = "Data Engineer",
-            slug         = "dataengineer",
-            description  = "CPU: 1 core, RAM: 2Gi"
-            kubespawner_override = {
-              cpu_limit = 1,
-              mem_limit = "2G",
+            display_name = "Configure seu Ambiente"
+            slug         = "jupyter-environment"
+            description  = "Escolha os recursos e a imagem para seu servidor Jupyter"
+            default      = true
+            profile_options = {
+              resources = {
+                display_name = "Recursos do Servidor"
+                choices = {
+                  minimal = {
+                    display_name = "Minimal"
+                    description  = "Para tarefas leves e exploração de dados"
+                    default      = true
+                    kubespawner_override = {
+                      cpu_guarantee = 0.5
+                      cpu_limit     = 2
+                      mem_guarantee = "2G"
+                      mem_limit     = "4G"
+                    }
+                  }
+                  standard = {
+                    display_name = "Standard"
+                    description  = "Ideal para análises e machine learning"
+                    kubespawner_override = {
+                      cpu_guarantee = 1
+                      cpu_limit     = 4
+                      mem_guarantee = "4G"
+                      mem_limit     = "16G"
+                    }
+                  }
+                  performance = {
+                    display_name = "Performance"
+                    description  = "Para modelos complexos e grandes datasets"
+                    kubespawner_override = {
+                      cpu_guarantee = 2
+                      cpu_limit     = 8
+                      mem_guarantee = "8G"
+                      mem_limit     = "32G"
+                    }
+                  }
+                }
+              }
+              image = {
+                display_name = "Imagem do Container"
+                choices = {
+                  datascience = {
+                    display_name = "Data Science"
+                    description  = "NumPy, Pandas, Scikit-learn, Matplotlib"
+                    default      = true
+                    kubespawner_override = {
+                      image = "quay.io/jupyter/datascience-notebook:python-3.11"
+                    }
+                  }
+                  tensorflow = {
+                    display_name = "TensorFlow"
+                    description  = "Deep Learning com TensorFlow e Keras"
+                    kubespawner_override = {
+                      image = "quay.io/jupyter/tensorflow-notebook:latest"
+                    }
+                  }
+                  pytorch = {
+                    display_name = "PyTorch"
+                    description  = "Deep Learning com PyTorch e Lightning"
+                    kubespawner_override = {
+                      image = "quay.io/jupyter/pytorch-notebook:latest"
+                    }
+                  }
+                  pyspark = {
+                    display_name = "PySpark"
+                    description  = "Processamento distribuído com Apache Spark"
+                    kubespawner_override = {
+                      image = "quay.io/jupyter/all-spark-notebook:spark-3.5.0"
+                    }
+                  }
+                }
+              }
             }
           }
         ]
